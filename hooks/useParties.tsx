a88e4e464a9b43c882a9a4122966b113837fd58b
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-09-12 15:08:57 +0200

    Update party name in useParties hook

--- hooks/useParties.tsx
+++ hooks/useParties.tsx
@@ -13,7 +13,7 @@ const DUMMY_PARTIES: Partial<Party>[] = [
     { id: '1', name: 'Flag and Sparrow', adress: 'Krakjice Marije 12', image: 'https://flagandsparrow.rs/assets/img/flag-and-sparrow-share.jpg' },
     { id: '2', name: 'KST', adress: 'Bulevara Kralja Aleksandra 123', image: 'https://best.rs/wp-content/uploads/2022/09/kst_baner-750x350-1.jpg' },
     { id: '3', name: 'KPTM', adress: 'Niksicka 14', image: 'https://belgrade-beat.com/photos/venues/123/c-1519416455.jpg' },
-    { id: '4', name: 'KPTM', adress: 'Kurca Moga 69', image: 'https://kafanapavlekorcagin.rs/assets/images/galerija/oblak/1a.jpg' },
+    { id: '4', name: 'Oblak u Pantalonama', adress: 'Kurca Moga 69', image: 'https://kafanapavlekorcagin.rs/assets/images/galerija/oblak/1a.jpg' },
 ]
 
 const NUMBER_OF_PARTIES = DUMMY_PARTIES.length;
